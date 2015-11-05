5271def813bbd621d5fb729c7de06c30ead5aefa
abdulla401 <abdulla.syd@gmail.com>
2015-11-05 11:43:58 +0000

    another commit before i check forms

--- public/js/question-directives.js
+++ public/js/question-directives.js
@@ -8,7 +8,7 @@ angularSpaceAppQuestionsDirectivesModule.directive("questionText" , function() {
   };
 });
 
-angularSpaceAppQuestionsDirectivesModule.directive("questionOption" , function() {
+angularSpaceAppQuestionsDirectivesModule.directive("questionRadio" , function() {
   return {
     restrict: 'E',
     transclude: true,
@@ -29,3 +29,25 @@ angularSpaceAppQuestionsDirectivesModule.directive("questionOption" , function()
     templateUrl: 'partials/directive/optionQuestion.html'
   };
 });
+
+angularSpaceAppQuestionsDirectivesModule.directive("questionCheckbox" , function() {
+	  return {
+	    restrict: 'E',
+	    transclude: true,
+	    scope: {
+	    	question: "=q",
+	    	updatedParent: "&m"
+	    },
+	    controller: ['$scope', function($scope) {
+	    	$scope.validateAnswer = function() {
+	    		$scope.question.validAnswer = true;
+	    		$scope.updatedParent();
+	    		}
+	    	$scope.saveAnswer = function() {
+	    		alert("This is the time to answer the question");
+	    	}
+	    ;
+	    }],
+	    templateUrl: 'partials/directive/checkboxQuestion.html'
+	  };
+	});
--- public/js/questions-all.js
+++ public/js/questions-all.js
@@ -40,53 +40,95 @@ angularSpaceAppQuestionsModule.factory('QuestionsFactory', ['$resource',
 
 
 
-var questions = [
-    {
-      id: "",
-      text: "Do you Smoke",
-      questionType: "radio",
-  	choices: [ { text: "yes", value: 0, displayIndex: 1 },   { text: "no", value: 1, displayIndex: 2 }],
-      errorMessage: "Choose either yes or no",
-      pattern: "",
-      validAnswer: false,
-      answer: "-1",
-      minLength: "",
-      maxLength: ""
-    },
-    {
-        id: "",
-        text: "Do you Drink",
-        questionType: "radio",
-    	choices: [ { text: "yes", value: 0, displayIndex: 1 },   { text: "no", value: 1, displayIndex: 2 }],
-        errorMessage: "Choose either yes or no",
-        pattern: "",
-        validAnswer: false,
-        answer: "-1",
-        minLength: "",
-        maxLength: ""
-      },
-  {
-    id: "",
-    text: "what is your name",
-    questionType: "text",
-    choices: [],
-    errorMessage: "name should be a text",
-    pattern: "",
-    answer: "",
-    minLength: "",
-    maxLength: ""
-  },
+var questions = [ {
+	id : "",
+	text : "How Many do you Smoke",
+	questionType : "checkbox",
+	choices : [ {
+		text : "1-4",
+		value : 0,
+		displayIndex : 1
+		
+	}, {
+		text : "5-10",
+		value : 1,
+		displayIndex : 2
+	} ,{
+		text : "More than 10",
+		value : 2,
+		displayIndex : 3
+	}
+	],
+	maxChoices: 2,
+	maxChoiceErrorMessage: "You cannot select more than 2",
+	minChoice: 1,
+	minChoiceMessage: "You need to select alteast 1",
+	errorMessage : "Choose either yes or no",
+	pattern : "",
+	validAnswer : false,
+	answer : "-1",
+	minLength : "",
+	maxLength : ""
+}, {
+	id : "",
+	text : "Do you Smoke",
+	questionType : "radio",
+	choices : [ {
+		text : "yes",
+		value : 0,
+		displayIndex : 1
+	}, {
+		text : "no",
+		value : 1,
+		displayIndex : 2
+	} ],
+	errorMessage : "Choose either yes or no",
+	pattern : "",
+	validAnswer : false,
+	answer : "-1",
+	minLength : "",
+	maxLength : ""
+}, {
+	id : "",
+	text : "Do you Drink",
+	questionType : "radio",
+	choices : [ {
+		text : "yes",
+		value : 0,
+		displayIndex : 1
+	}, {
+		text : "no",
+		value : 1,
+		displayIndex : 2
+	} ],
+	errorMessage : "Choose either yes or no",
+	pattern : "",
+	validAnswer : false,
+	answer : "-1",
+	minLength : "",
+	maxLength : ""
+}, {
+	id : "",
+	text : "what is your name",
+	questionType : "text",
+	choices : [],
+	errorMessage : "name should be a text",
+	pattern : "",
+	answer : "",
+	minLength : "",
+	maxLength : ""
+},
 
-  {
-    id: "",
-    text: "what is your name",
-    questionType: "text",
-    choices: [],
-    errorMessage: "name should be a text",
-    pattern: "",
-    answer: "",
-    minLength: "",
-    maxLength: ""
-  }
-  
+{
+	id : "",
+	text : "what is your name",
+	questionType : "text",
+	choices : [],
+	errorMessage : "name should be a text",
+	pattern : "",
+	answer : "",
+	minLength : "",
+	maxLength : ""
+}
+
 ];
